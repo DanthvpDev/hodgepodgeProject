a7ec992f74302e08eb8a135056fe984c3c281fd1
Daniel Hernández <danthdev02@gmail.com>
2024-11-27 23:34:27 -0600

    Index finished, carrusel initialized

---
+++ scripts/carrusel.js
@@ -0,0 +1,45 @@
+let fotosComida = [
+    {
+        nombre: 'Spaghetti con Camarones al ajillo',
+        foto: 'https://images.pexels.com/photos/3009323/pexels-photo-3009323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        descripcion: null,
+        porciones: null,
+        costo: 0,
+    },
+    {
+        nombre: 'Pizza',
+        foto: 'https://images.pexels.com/photos/5993865/pexels-photo-5993865.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        descripcion: null,
+        porciones: null,
+        costo: 0,
+    },
+    {
+        nombre: 'Lasagna',
+        foto: 'https://images.unsplash.com/photo-1709429790175-b02bb1b19207?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: null,
+        porciones: null,
+        costo: 0,
+    },
+    {
+        nombre: 'Ravioli',
+        foto: 'https://plus.unsplash.com/premium_photo-1701013639320-71a02c6ddd8f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: null,
+        porciones: null,
+        costo: 0,
+    },
+    {
+        nombre: 'Vino Tinto',
+        foto: 'https://images.unsplash.com/photo-1653697739273-de3ffa3d9ffe?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: null,
+        porciones: null,
+        costo: 0,
+    },
+    {
+        nombre: '',
+        foto: '',
+        descripcion: null,
+        porciones: null,
+        costo: 0,
+    },
+
+]

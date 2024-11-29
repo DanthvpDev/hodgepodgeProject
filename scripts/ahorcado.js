3974aa137cc9a6a590a99ea898a4b5fa77d2ad78
Daniel Hernández <danthdev02@gmail.com>
2024-11-28 19:48:36 -0600

    Juego del ahorcado almost finished

--- scripts/ahorcado.js
+++ scripts/ahorcado.js
@@ -1,53 +1,62 @@
-const contendorLetras = document.getElementById('contenedorLetras');
-const contenedorPalabra = document.getElementById('contenedorPalabra');
-const btnLetras = document.getElementsByClassName('letrasTeclado');
-const letrasAhorcado = document.getElementsByClassName('letraCuadroAhorcado');
-const pildoraFallos = document.getElementById('fallos') ;
-
-let palabraActual;
-let letraCorrecta;
-let letrasSeleccionadas = [];
-let contadorFallos = 0;
-let maxFallos;
-
-const alfabetoEspaniol = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ",
-    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const arregloPalabras = ['gustago'];
-
-function GenerarLetras(arregloAlfabeto) {
-    arregloAlfabeto.forEach(element => {
-        let letras = document.createElement('p');
-        letras.innerHTML = element;
-        letras.classList = "uppercase px-6 py-3 rounded-lg bg-[#FF8800] w-max font-semibold text-slate-900 hover:bg-[#fbad53] hover:transition-colors transition-colors cursor-pointer letrasTeclado";
-        contendorLetras.appendChild(letras);
-    });
-}
-
-function GenerarEspacioPalabra(arregloPalabras) {
-    // let random = Math.abs(Math.random() * arregloPalabras.length);
-    let random = 0;
-    let palabraUnica = arregloPalabras[random].split('');
-    palabraActual = palabraUnica;
-    palabraUnica.forEach(element => {
-        let letras = document.createElement('p');
-        letras.innerHTML = element;
-        letras.classList = "uppercase px-6 py-1 text-transparent w-max font-semibold hover:transition-colors transition-colors cursor-pointer border-b-2 border-b-[#FF8800] text-2xl h-max letraCuadroAhorcado";
-        contenedorPalabra.appendChild(letras);
-    });
-}
-
-function EvaluarLetra(letra, arregloPalabra) {
-    letraCorrecta = arregloPalabra.filter(e => e.toLowerCase() == letra.toLowerCase());
-    return letraCorrecta.length > 0 ? true : false;
-
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    maxFallos = Math.round(palabraActual.length / 1.5);
-    if(contadorFallos < maxFallos) {
+
+    const contendorLetras = document.getElementById('contenedorLetras');
+    const contenedorPalabra = document.getElementById('contenedorPalabra');
+    const btnLetras = document.getElementsByClassName('letrasTeclado');
+    const btnReiniciar = document.getElementById('btnReiniciar');
+    const letrasAhorcado = document.getElementsByClassName('letraCuadroAhorcado');
+    const pildoraFallos = document.getElementById('fallos');
+    let fotosAhorcado = [
+        { nombre: 1, foto: '1.png' },
+        { nombre: 2, foto: '2.png' },
+        { nombre: 3, foto: '3.png' },
+        { nombre: 4, foto: '4.png' },
+        { nombre: 5, foto: '5.png' },
+        { nombre: 6, foto: '6.png' },
+        { nombre: 7, foto: '7.png' }
+    ];
+
+    let palabraActual;
+    let letraCorrecta;
+    let letrasSeleccionadas = [];
+    let contadorFallos;
+    let aciertos;
+
+    const alfabetoEspaniol = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ",
+        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const arregloPalabras = [
+        "elefante",
+        "manzana",
+        "montaña",
+        "bicicleta",
+        "juguete",
+        "ciudad",
+        "cancion",
+        "lagartija",
+        "cuchara",
+        "relampago",
+        "camiseta",
+        "carrera",
+        "pelicula",
+        "biblioteca",
+        "tiburon",
+        "maleta",
+        "universo",
+        "helado",
+        "arbol",
+        "espejo"
+    ];
+
+    function GenerarLetras(arregloAlfabeto) {
+        arregloAlfabeto.forEach(element => {
+            let letras = document.createElement('p');
+            letras.innerHTML = element;
+            letras.classList = "uppercase px-6 py-3 rounded-lg bg-[#FF8800] w-max font-semibold text-slate-900 hover:bg-[#fbad53] hover:transition-colors transition-colors cursor-pointer letrasTeclado";
+            contendorLetras.appendChild(letras);
+        });
+
         [...btnLetras].forEach(element => {
             element.addEventListener('click', e => {
-    
                 //* Verifica que la letra no se haya seleccionado
                 if (!letrasSeleccionadas.includes(e.target.innerHTML)) {
                     letrasSeleccionadas.push(e.target.innerHTML);
@@ -56,34 +65,112 @@ document.addEventListener('DOMContentLoaded', () => {
                     //* Evalúa si la letra es correcta o incorrecta
                     if (EvaluarLetra(e.target.innerHTML, palabraActual)) {
                         e.target.classList.toggle('bg-[#39FF14]');
-                        
+    
                         //?Muestra la letra del cuadro de ahorcado
                         let elementoCorrectoCuadroAhorcado = [...letrasAhorcado].filter(element => element.innerHTML.toLowerCase() == letraCorrecta[0]);
-                        console.log(elementoCorrectoCuadroAhorcado)
                         elementoCorrectoCuadroAhorcado.forEach(element => {
                             element.classList.toggle('text-transparent');
                             element.classList.toggle('text-[#379cc8]');
                             element.classList.toggle('bg-slate-900');
                             element.classList.toggle('rounded-lg');
                         });
+    
+                        aciertos += elementoCorrectoCuadroAhorcado.length;
+                        console.log(aciertos)
                     }
                     else {
                         contadorFallos++;
-                        pildoraFallos.textContent = `Fallos: ${contadorFallos} / ${maxFallos}` 
+                        pildoraFallos.textContent = `Fallos: ${contadorFallos} / ${fotosAhorcado.length}`
                         e.target.classList.toggle('bg-[#ff006e]');
                     }
+    
+                    if (EsGanador(palabraActual.length, aciertos)) {
+                        TerminarJuego('Ganador');
+                    };
+    
+                    if (EsPerdedor(contadorFallos, fotosAhorcado.length)) {
+                        TerminarJuego('Perdedor');
+                    }
+    
                 }
                 else {
                     alert('Ya se seleccionó esta letra')
                 }
+    
             })
         });
     }
 
+    function GenerarEspacioPalabra(arregloPalabras) {
+        let random = Math.floor(Math.random() * arregloPalabras.length);
+        let palabraUnica = arregloPalabras[random].split('');
+        palabraActual = palabraUnica;
+        palabraUnica.forEach(element => {
+            let letras = document.createElement('p');
+            letras.innerHTML = element;
+            letras.classList = "uppercase px-6 py-1 text-transparent w-max font-semibold hover:transition-colors transition-colors cursor-pointer border-b-2 border-b-[#FF8800] text-2xl h-max letraCuadroAhorcado";
+            contenedorPalabra.appendChild(letras);
+        });
+    }
+
+    function EvaluarLetra(letra, arregloPalabra) {
+        letraCorrecta = arregloPalabra.filter(e => e.toLowerCase() == letra.toLowerCase());
+        return letraCorrecta.length > 0 ? true : false;
+
+    }
+
+    function EsGanador(longitudPalabra, aciertos) {
+        return aciertos == longitudPalabra ? true : false;
+    }
+
+    function EsPerdedor(intentos, cantidadMaximaFallos) {
+        return intentos == cantidadMaximaFallos ? true : false;
+    }
+
+    function TerminarJuego(mensaje) {
+        contenedorPalabra.classList.toggle('hidden');
+        contendorLetras.classList.toggle('hidden');
+        pildoraFallos.classList.toggle('hidden');
+        alert(mensaje);
+    }
+
+    function IniciarJuego() {
+        [...btnLetras].forEach(element => {
+            element.classList.remove('text-[#379cc8]');
+            element.classList.remove('bg-slate-900');
+            element.classList.remove('rounded-lg');
+            element.classList.remove('bg-[#39FF14]');
+            element.classList.toggle('hover:bg-[#fbad53]');
+            element.classList.remove('bg-[#ff006e]');
+
+        });
+        contenedorPalabra.classList.remove('hidden');
+        contendorLetras.classList.remove('hidden');
+        pildoraFallos.classList.remove('hidden');
+
+        if (letrasAhorcado.length > 0) {
+            contenedorPalabra.innerHTML = '';
+        }
+
+        if (btnLetras.length > 0) {
+            [...btnLetras].forEach(element => element.remove());
+        }
+
+        aciertos = 0;
+        contadorFallos = 0;
+        letrasSeleccionadas.length = [];
+        palabraActual = undefined;
+        letraCorrecta = undefined;
+
+        GenerarEspacioPalabra(arregloPalabras);
+        GenerarLetras(alfabetoEspaniol);
+
+    }
+
+
+
+    btnReiniciar.addEventListener('click', IniciarJuego, false);
+
+    IniciarJuego();
+
 })
-
-
-
-
-GenerarLetras(alfabetoEspaniol);
-GenerarEspacioPalabra(arregloPalabras);
